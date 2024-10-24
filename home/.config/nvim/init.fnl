ffbb844c7815b6ed5610abb194c90839af4cc009
Raja Gangopadhya <rgangopadhya@ucla.edu>
2024-10-24 15:21:45 -0700

    Switch to nfnl

---
+++ home/.config/nvim/init.fnl
@@ -0,0 +1,2 @@
+(require :config.bootstrap)
+(require :config.lazy)
--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,2 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- [nfnl] Compiled from init.fnl by https://github.com/Olical/nfnl, do not edit.
+require("config.bootstrap")
+return require("config.lazy")
